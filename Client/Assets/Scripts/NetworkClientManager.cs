10262a2129c669aca15e82b35664258104ca418f
Zen <wolfskyzen@gmail.com>
2015-04-10 22:46:20 -0700

    Project Resubmission
    Resubmitting the projects, but WITH the proper gitignore in place first!

---
+++ Client/Assets/Scripts/NetworkClientManager.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+using System.Linq;
+
+public class NetworkClientManager : MonoBehaviour
+{
+	//private static string GAMEMODENAME = "TESTMODE";
+	private static string GAMETYPENAME = "VFCONGAME";
+
+	private static NetworkClientManager sInstance = null;
+	public static NetworkClientManager Instance
+	{
+		get { return sInstance; }
+	}
+	
+	public delegate void StateChange(ConnectionState state);
+	public static StateChange StateChanged;
+	
+	public enum ConnectionState
+	{
+		Failed			= -1,
+		Disconnected	= 0,
+		Connecting,
+		Connected
+	}
+	
+	[DefaultValue(ConnectionState.Disconnected)]
+	public ConnectionState State { get; private set; }
+	
+	public bool IsConnected
+	{
+		get { return (State == ConnectionState.Connected); }
+	}
+	
+	void Awake()
+	{
+		if(null == sInstance)
+		{
+			sInstance = this;
+		}
+
+		InvokeRepeating("Tick", 0.0f, 1.0f);
+	}
+	
+	void Connect()
+	{
+		SetState(ConnectionState.Connecting);
+		try
+		{
+			MasterServer.RequestHostList(GAMETYPENAME);
+			var hosts = MasterServer.PollHostList();
+			if(null == hosts || hosts.Length == 0)
+			{
+				Debug.Log("Connection error: No hosts found");
+				SetState(ConnectionState.Disconnected);
+				return;
+			}
+
+			// Only attempt the first host
+			var targetHost = hosts.SingleOrDefault();
+			if(null == targetHost)
+			{
+				Debug.LogError("Connection error: Host entry is invalid!");
+				SetState(ConnectionState.Disconnected);
+				return;
+			}
+
+			Debug.LogFormat("Connecting to: {0} {1}", targetHost.gameType, targetHost.gameName);
+			var result = Network.Connect(targetHost);
+			if(NetworkConnectionError.NoError != result)
+			{
+				Debug.Log("Connection error: " + result.ToString());
+				SetState(ConnectionState.Disconnected);
+			}
+		}
+		catch(UnityException ex)
+		{
+			Debug.LogError("Connection exception: " + ex.ToString());
+			SetState(ConnectionState.Failed);
+		}
+	}
+	
+	void Disconnect()
+	{
+		if(State == ConnectionState.Connected || State == ConnectionState.Connecting)
+		{
+			SetState(ConnectionState.Disconnected);
+			try
+			{
+				Network.Disconnect();
+			}
+			catch(UnityException ex)
+			{
+				Debug.LogError("Error on disconnect: " + ex.ToString());
+			}
+		}
+	}
+	
+	void OnApplicationQuit()
+	{
+		CancelInvoke("Tick");
+
+		if(null != sInstance)
+		{
+			Disconnect();
+			sInstance = null;
+		}
+	}
+
+	void OnConnectedToServer()
+	{
+		if(State == ConnectionState.Connecting)
+		{
+			SetState(ConnectionState.Connected);
+		}
+		else
+		{
+			Debug.LogError("Connected in bad state!");
+			Disconnect();
+		}
+	}
+	
+	void OnDisconnectedFromServer(NetworkDisconnection info)
+	{
+		if(State != ConnectionState.Disconnected)
+		{
+			Debug.Log("Disconnected from server: " + info.ToString());
+			SetState(ConnectionState.Disconnected);
+		}
+	}
+
+	void OnFailedToConnect(NetworkConnectionError error)
+	{
+		if(State != ConnectionState.Connecting)
+		{
+			Debug.LogError("Failed to connect in bad state!");
+		}
+
+		Debug.Log("Connection error: " + error.ToString());
+		SetState(ConnectionState.Disconnected);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent mse)
+	{
+		Debug.Log("Master Server Event: " + mse.ToString());
+	}
+
+	void SetState(ConnectionState state)
+	{
+		if(state == State)
+		{
+			return;
+		}
+		
+		Debug.Log("Client state: " + state.ToString());
+		State = state;
+		if(null != StateChanged)
+		{
+			StateChanged(state);
+		}
+	}
+
+	void Tick()
+	{
+		if(ConnectionState.Disconnected == State)
+		{
+			Connect();
+		}
+	}
+}
---
+++ PhoneClient/Assets/Scripts/LocationManager.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.ComponentModel;
+
+public class LocationManager : MonoBehaviour
+{
+	private static LocationManager sInstance;
+	public static LocationManager Instance
+	{
+		get { return sInstance; }
+	}
+
+	static readonly float kUpdateDelay = 1.0f;
+	private float mDelayTime = 0.0f;
+
+	public enum ListeningState
+	{
+		Disabled	= -1,
+		Stopped		= 0,
+		Starting,
+		Running
+	};
+
+	public delegate void LocationStateChanged(ListeningState state);
+	public static event LocationStateChanged OnLocationStateChanged;
+
+	public delegate void LocationUpdate(LocationInfo info);
+	public static event LocationUpdate OnLocationUpdate;
+
+	[DefaultValue(ListeningState.Stopped)]
+	ListeningState State { get; set; }
+
+	void Awake()
+	{
+		if(null == sInstance)
+		{
+			sInstance = this;
+		}
+	}
+
+	void OnApplicationDestroyed()
+	{
+		if(ListeningState.Starting == State || ListeningState.Running == State)
+		{
+			try
+			{
+				Input.location.Stop();
+			}
+			catch(UnityException ex)
+			{
+				Debug.LogError("Failed to stop location: " + ex.ToString());
+			}
+			SetState(ListeningState.Stopped);
+		}
+
+		if(this == sInstance)
+		{
+			sInstance = null;
+		}
+	}
+
+	private void SetState(ListeningState state)
+	{
+		if(state == State)
+		{
+			return;
+		}
+
+		Debug.Log("GPS State: " + state);
+		State = state;
+
+		if(null != OnLocationStateChanged)
+		{
+			OnLocationStateChanged(State);
+		}
+	}
+
+	void Update()
+	{
+		mDelayTime -= Time.deltaTime;
+		if(mDelayTime <= 0.0f)
+		{
+			mDelayTime += kUpdateDelay;
+			UpdateTick();
+		}
+	}
+
+	void UpdateTick()
+	{
+		if(!Input.location.isEnabledByUser)
+		{
+			switch(State)
+			{
+				case ListeningState.Disabled:
+					// Do nothing
+				break;
+
+				case ListeningState.Stopped:
+				{
+					SetState(ListeningState.Disabled);
+				}
+				break;
+
+				case ListeningState.Starting:
+				case ListeningState.Running:
+				{
+					Input.location.Stop();
+					try
+					{
+						Input.location.Stop();
+					}
+					catch(UnityException ex)
+					{
+						Debug.LogError("Failed to stop location: " + ex.ToString());
+					}
+					SetState(ListeningState.Disabled);
+				}
+				break;
+			}
+		}
+		else
+		{
+			switch(State)
+			{
+				case ListeningState.Disabled:
+				{
+					SetState(ListeningState.Stopped);
+				}
+				break;
+
+				case ListeningState.Stopped:
+				{
+					try
+					{
+						Input.location.Start(5.0f, 0.5f);
+						SetState(ListeningState.Starting);
+					}
+					catch(UnityException ex)
+					{
+						Debug.LogError("Failed to start location: " + ex.ToString());
+					}
+				}
+				break;
+
+				case ListeningState.Starting:
+				{
+					if(LocationServiceStatus.Running == Input.location.status)
+					{
+						SetState(ListeningState.Running);
+					}
+					else if (LocationServiceStatus.Initializing != Input.location.status)
+					{
+						Debug.LogError("Failed to initalize location");
+						SetState(ListeningState.Stopped);
+					}
+				}
+				break;
+
+				case ListeningState.Running:
+				{
+					// TODO: Determine ping?
+					// TODO: Track last update time!
+					if(null != OnLocationUpdate)
+					{
+						OnLocationUpdate(Input.location.lastData);
+					}
+				}
+				break;
+			}
+		}
+	}
+}
