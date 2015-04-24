ff39a17d1a94e8d898c73afe40f7d8020834e98b
Zen <wolfskyzen@gmail.com>
2015-04-24 13:36:07 -0700

    PlayerDot Prefab and Dynamic Sprites
    Created a prefab for the player dots
    Setup the gameplay client manager to dynamically create a user marker
    using the prefab player dot.
    Also, learned how resource loading and dynamic creation works! :+1:

--- Client/Assets/Scripts/GameplayClientManager.cs
+++ Client/Assets/Scripts/GameplayClientManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GameplayClientManager : MonoBehaviour
 {
@@ -9,10 +10,48 @@ public class GameplayClientManager : MonoBehaviour
 	private NetworkView mView = null;
 	private Vector2 mLastPosition = new Vector2();
 
+	private GameObject mCanvas = null;
+	private CanvasScaler mCanvasScaler = null;
+	private GameObject mUserMarker = null;
+
+	private GameObject mPlayerDotPrefab = null;
+
+	private Dictionary<string, GameObject> mClientSprites = new Dictionary<string, GameObject>();
+
 	void Awake()
 	{
 		NetworkClientManager.StateChanged += HandleStateChanged;
 		mView = this.gameObject.GetComponent<NetworkView>();
+
+		mPlayerDotPrefab = Resources.Load("PlayerDot") as GameObject;
+		if(null == mPlayerDotPrefab)
+		{
+			Debug.LogError("Unable to load PlayerDot prefab!");
+		}
+
+		mCanvas = GameObject.Find("Canvas");
+		if(null != mCanvas)
+		{
+			mCanvasScaler = mCanvas.GetComponent<CanvasScaler>();
+			if(null != mCanvasScaler && null != mPlayerDotPrefab)
+			{
+				mUserMarker = Instantiate(mPlayerDotPrefab) as GameObject;
+				if(null != mUserMarker)
+				{
+					mUserMarker.name = "LocalPlayer";
+					var renderer = mUserMarker.GetComponent<SpriteRenderer>();
+					if(null != renderer)
+					{
+						// Make it orange!
+						renderer.color = new Color(1, 0.6f, 0);
+					}
+				}
+				else
+				{
+					Debug.LogError("Unable to create local player dot");
+				}
+			}
+		}
 	}
 
 	void HandleStateChanged(NetworkClientManager.ConnectionState state)
@@ -31,6 +70,11 @@ public class GameplayClientManager : MonoBehaviour
 	void OnApplicationQuit()
 	{
 		CancelInvoke("Tick");
+		if(null != mUserMarker)
+		{
+			Destroy(mUserMarker);
+			mUserMarker = null;
+		}
 	}
 
 	void Reset()
@@ -65,6 +109,11 @@ public class GameplayClientManager : MonoBehaviour
 		Vector2 clientPos = new Vector2(latitute, longitude);
 		if((mLastPosition - clientPos).sqrMagnitude > MaxSearchRadiusSqr)
 		{
+			if(mClientSprites.ContainsKey(info.sender.guid))
+			{
+				//RemoveClient(info.sender.guid);
+			}
+
 			// Too far away!
 			return;
 		}
